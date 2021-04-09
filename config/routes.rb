84d2a426a677e48d240037562542d3b5a6f496ee
Snake <killer9911@mail.ru>
2021-04-09 18:03:08 +0300

    Add gem 'jsonapi-serializer'

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@
 
 Rails.application.routes.draw do
   root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
