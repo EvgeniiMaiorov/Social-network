593584935a13cee8df3931f5ae5233406bead9a2
Snake <killer9911@mail.ru>
2021-04-29 00:27:20 +0300

    Add interests and categories
    Where?
    https://trello.com/c/rZIh5Wk0/11-add-interest-category-model
    
    What?
    In order for the user to select interests divided into categories, we have to create interests and interest categories
    
    How?
    Create interest category model and respective migration
    Create interest model and respective migration
    Create user interest model and respective migration
    Add interest_id column to users table by creating respective migration
    Add relation between models

---
+++ db/migrate/20210428194731_create_interest_categories.rb
@@ -0,0 +1,12 @@
+class CreateInterestCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :interest_categories do |t|
+      t.string :category_name
+      t.integer :interest_id
+
+      t.timestamps
+    end
+
+    add_index :interest_categories, :category_name, unique: true
+  end
+end
