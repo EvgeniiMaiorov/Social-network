49ba8556a69748443a6f559d912d50bb655adc3a
Snake <killer9911@mail.ru>
2021-04-29 16:11:55 +0300

    Add seeds in db
    What?
    In order for the user to select interests fill in the database seeds whith data
    
    How?
    Add data in seeds.rb file
    
    Fix
    Fix relation between interest and interest category models

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Interest < ApplicationRecord
-  belongs_to :category
+  belongs_to :interest_category
   has_many :user_interests, dependent: :destroy
   has_many :users, through: :user_interests
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,104 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+interest_categories = %w[
+  Cars Pets Books Art Movies Serials Beauty Sport
+  Active\ rest Video\ games Games
+]
+interest_categories.each do |interest_category|
+  InterestCategory.create!(category_name: interest_category)
+end
+
+cars_interests = %w[
+  Acura Audi BMW Cadillac Chrysler Citroen Dodge Fiat Ford
+  Geely Honda Hyundai Infiniti Jaguar Jeep Kia Lexus Mazda
+  Mercedes-Benz Mitsubishi Nissan Opel Peugeot Porsche Renault
+  Saab SEAT Skoda Subaru Toyota Volkswagen Volvo
+]
+cars_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Cars').interests.create!(name: interest)
+end
+
+pets_interests = %w[
+  Dogs Cats Horses Fishes Mini\ pigs Spiders Hamsters Rabbits
+  Parrots Reptiles
+]
+pets_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Pets').interests.create!(name: interest)
+end
+
+beauty_interests = %w[
+  Fantasy Adventure Romance Contemporary Dystopian Mystery Horror
+  Thriller Paranormal Historical Science Fiction Memoir Cooking
+  Art Development Motivational Health History Travel
+  Families\ &\ Relationships Humor Children’s
+]
+beauty_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Books').interests.create!(name: interest)
+end
+
+arts_interests = %w[
+  Abstract\ Expressionism Art\ Deco Art\ Nouveau Avant-garde Baroque Bauhaus
+  Classicism Conceptual\ Art Constructivism Cubism Dada\ /\ Dadaism Expressionism
+  Fauvism Futurism Impressionism Installation\ Art Land\ Art Minimalism
+  Neo-Impressionism Neoclassicism Performance\ Art Pop\ Art Post-Impressionism
+  Precisionism Rococo Surrealism Suprematism Symbolism
+]
+arts_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Art').interests.create!(name: interest)
+end
+
+films_interests = %w[
+  Action Comedy Drama Fantasy Horror Mystery Romance Thriller
+  Western Sport
+]
+films_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Movies').interests.create!(name: interest)
+end
+
+serials_interests = %w[
+  Drama History Thriller Detective Action Comedy Crime Melodrama
+  Adventure Sport Horror Fantasy Mystery
+]
+serials_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Serials').interests.create!(name: interest)
+end
+
+beauty_interests = %w[
+  Make\ up Skin\ care Fashion Style Hairstyle Hair\ care Nails Fitness
+  Proper\ nutrition
+]
+beauty_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Beauty').interests.create!(name: interest)
+end
+
+sport_interests = %w[
+  Tennis Volleyball Basketball Baseball Criket Skateboarding Frisbee
+  Climbing Cycling Boxing Football Golf Gymnastic Running Skiing Hockey Lacrosse Polo Rafting Racing Quidditch
+]
+sport_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Sport').interests.create!(name: interest)
+end
+
+active_rest_interests = %w[
+  Rafting Surfing Diving Water\ skiing Swimming Skiing Skating Snowboarding
+  Skydiving Paragliding Cycling Mountain\ Bike Carting Horseback\ riding
+]
+active_rest_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Active rest').interests.create!(name: interest)
+end
+
+video_games_interests = %w[
+  Action Adventure Role-playing Simulation Strategy Sports Puzzle Idle
+  On-line Casual Shooter Fighting Escape
+]
+video_games_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Video games').interests.create!(name: interest)
+end
+
+games_interests = %w[
+  Board\ games Card\ games Dice\ games Pencil-and-paper\ games Singing\ games Eurogames
+  Tile-based\ games Pong Drinking\ games
+]
+games_interests.each do |interest|
+  InterestCategory.find_by(category_name: 'Games').interests.create!(name: interest)
+end
