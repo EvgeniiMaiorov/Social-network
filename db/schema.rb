47bc05656ffb65134fd1a50273490ec09d34b66a
Snake <killer9911@mail.ru>
2021-04-30 16:17:11 +0300

    Add invitation model
    Where?
    https://trello.com/c/IiHvTl7b/19-add-invitations
    
    What?
    In order for the user to send friendship invitations, we have to create an invitation model
    
    How?
    Create invitation model and respective migrations
    Add relations between user and invitation models
    Add friend methods to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_205308) do
+ActiveRecord::Schema.define(version: 2021_04_30_120821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2021_04_28_205308) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.boolean "confirmed", default: false
+    t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
@@ -66,4 +73,5 @@ ActiveRecord::Schema.define(version: 2021_04_28_205308) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "invitations", "users"
 end
